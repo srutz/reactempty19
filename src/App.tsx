0fbec671552810b1634d155d46635bc29f349fb5
stepan.rutz <stepan.rutz@gmx.de>
2025-05-13 12:14:16 +0200

    slow without react compiler or without memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, type MouseEvent } from "react";
+import { memo, useState, type MouseEvent } from "react";
 import { cn } from "./lib/utils";
 import { Palette } from "./Palette";
 
@@ -13,11 +13,12 @@ export function App() {
         <div className={cn("bg-gray-100 h-1 grow flex flex-col justify-center")}>
             <div className="self-center text-sm">Mouse Position {pos.x}, {pos.y}</div>
             <div className="self-center p-4 bg-white shadow-lg flex flex-col items-center justify-center"
-                onMouseMove={handleMouseMove}>
-                <Palette size={30} />
+                    onMouseLeave={() => setPos({ x: -1, y: -1})}
+                    onMouseMove={handleMouseMove}>
+                <Palette size={50} />
             </div>
         </div>
     )
 }
 
-
+const MemoPalette = memo(Palette)
--- src/Palette.tsx
+++ src/Palette.tsx
@@ -1,15 +1,18 @@
 
-/* render x * y rectangles in a grid  and color them to make a nice rgb color palette */
-
 import { cn } from "./lib/utils"
+
+/* 
+ * example of an EXPENSIVE component
+ * render x * y rectangles in a grid  and color them to make a nice rgb color palette
+ */
 
 
 export function Palette({size, className} : { size: number, className?: string }) {
-    const w = 16
-    const h = 16
+    const w = 6
+    const h = 6
     return (
         <div style={{ gridTemplateColumns: "repeat(" + size + ", 1fr)"}}
-            className={cn("self-center grid gap-1", 
+            className={cn("self-center grid gap-[1px]", 
                 "w-[" + w * size + "px]", 
                 "h-[" + h * size + "px]", 
                 className)} >
