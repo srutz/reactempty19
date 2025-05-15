d490ad82467b52907331307330b9d782968a86f2
stepan.rutz <stepan.rutz@gmx.de>
2025-05-15 11:48:02 +0200

    react compiler config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,20 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+declare const console: any;
+
 
 export default defineConfig({
     plugins: [
         react({
             babel: {
                 plugins: [
-                    //["babel-plugin-react-compiler", {}}],
+                    ["babel-plugin-react-compiler", {
+                        sources: (filename: any) => {
+                            console.log("filename", filename)
+                            return true
+                        }
+                    }],
                 ],
             },
         }),
