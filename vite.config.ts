187fedd5cf42fc1cb5a6eab4a837ea7e34035bfc
stepan.rutz <stepan.rutz@gmx.de>
2025-05-15 15:45:13 +0200

    disable compiler

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,14 @@ export default defineConfig({
         react({
             babel: {
                 plugins: [
+                    /*
                     ["babel-plugin-react-compiler", {
                         sources: (filename: any) => {
                             console.log("filename", filename)
                             return true
                         }
                     }],
+                    */
                 ],
             },
         }),
