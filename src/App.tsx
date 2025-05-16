9723f5d020da9b1005740dd1a18e3b2c1793fe45
stepan.rutz <stepan.rutz@gmx.de>
2025-05-16 09:48:37 +0200

    included sheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 
 export function App() {
     return (
         <div className="grow m-8 bg-green-400">
-            
+            <HelloSheet></HelloSheet>
         </div>
+    )
+}
+
+function HelloSheet() {
+    return (
+        <Sheet>
+            <SheetTrigger>Open</SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Are you absolutely sure?</SheetTitle>
+                    <SheetDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </SheetDescription>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
     )
 }
 
