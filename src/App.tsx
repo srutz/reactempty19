6b6599e4219db9615e41c7f91b26a87913ae604b
stepan.rutz <stepan.rutz@gmx.de>
2025-05-27 10:07:40 +0200

    quotes loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+
+export interface Quote {
+    id: number,
+    quote: string,
+    author: string
+}
+
+export interface QuotesResponse {
+    total: number; skip: number; limit: number;
+    quotes: Quote[]
+}
 
 export function App() {
+    const { data } = useQuery({
+        queryKey: [ "key1" ],
+        queryFn: async() => {
+            const response = await fetch("https://dummyjson.com/quotes")
+            const data = await response.json()
+            return data as QuotesResponse
+        } 
+    })
+    console.log("App", data)
     return (
-        <div className="grow m-8 bg-green-400">
+        <div className="h-1 grow m-8 bg-green-400 overflow-auto flex flex-col">
+            <pre className="text-sm">
+                {JSON.stringify(data, null, 4)}
+            </pre>
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { App } from './App.tsx'
+import './index.css'
 
+const client = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={client}>    
+        <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
