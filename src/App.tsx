0f5db1392deec408f9de468ec5a1e9b931f13019
stepan.rutz <stepan.rutz@gmx.de>
2025-05-27 08:37:30 +0200

    react compiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import { Palette } from "./Palette";
 
 export function App() {
+    const [coords, setCoords] = useState({x: -1, y: -1})
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        setCoords({ x: e.clientX, y: e.clientY })
+    }
     return (
-        <div className="grow m-8 bg-green-400">
+        <div className="grow m-8 bg-green-400 flex flex-col gap-2" onMouseMove={handleMouseMove}>
+            <div>Coords: {coords.x} x {coords.y}</div>
+            <Palette size={70} />
         </div>
     )
 }
