ea3263f8d27d3b86e2b18e8c7ac79cab4de77538
stepan.rutz <stepan.rutz@gmx.de>
2025-05-28 10:35:47 +0200

    3 fields

--- src/App.tsx
+++ src/App.tsx
@@ -22,13 +22,25 @@ function useStateWithLocalStorage<T>(key: string, initializer: T|(() => T)) {
 export function App() {
     const [age, setAge] = useStateWithLocalStorage("input-age", 27)
     const [name, setName] = useStateWithLocalStorage("input-name2", () => "ppp")
+    const [email, setEmail] = useStateWithLocalStorage("input-email", () => "")
     console.log("rerender App", name)
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)
+    const handleChange1 = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)
+    const handleChange2 = (e: ChangeEvent<HTMLInputElement>) => 
+        setAge(Number.parseInt(e.target.value))
+    const handleChange3 = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
     return (
-        <div className="grow m-8 ">
+        <div className="grow m-8 flex flex-col gap-2">
             <label htmlFor="i1">Name</label>
             <Input id="i1" value={name} 
-                onChange={handleChange} 
+                onChange={handleChange1} 
+                />
+            <label htmlFor="i2">Age2</label>
+            <Input id="i2" value={age} type="number"
+                onChange={handleChange2} 
+                />
+            <label htmlFor="i3">E-Mail</label>
+            <Input id="i3" value={email} type="email"
+                onChange={handleChange3} 
                 />
         </div>
     )
