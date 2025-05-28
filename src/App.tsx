cbe80513748504cef95edf7cfee9df25b623f976
stepan.rutz <stepan.rutz@gmx.de>
2025-05-28 10:52:07 +0200

    toasting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState, type ChangeEvent } from "react";
+import { toast } from "sonner";
 import { Input } from "./components/ui/input";
+import { Toaster } from "./components/ui/sonner";
 
-function useStateWithLocalStorage<T>(key: string, initializer: T|(() => T)) {
-    const [ value, setValue ] = useState(() => {
+function useStateWithLocalStorage<T>(key: string, initializer: T | (() => T)) {
+    const [value, setValue] = useState(() => {
         // initialisier-funktion
         let v = localStorage.getItem(key)
         if (v) {
@@ -16,32 +18,44 @@ function useStateWithLocalStorage<T>(key: string, initializer: T|(() => T)) {
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(value))
     }, [value])
-    return [ value, setValue ] as const
+    return [value, setValue] as const
 }
 
 export function App() {
     const [age, setAge] = useStateWithLocalStorage("input-age", 27)
     const [name, setName] = useStateWithLocalStorage("input-name2", () => "ppp")
     const [email, setEmail] = useStateWithLocalStorage("input-email", () => "")
-    console.log("rerender App", name)
+    console.log("rerender App", name, age, email)
     const handleChange1 = (e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)
-    const handleChange2 = (e: ChangeEvent<HTMLInputElement>) => 
+    const handleChange2 = (e: ChangeEvent<HTMLInputElement>) =>
         setAge(Number.parseInt(e.target.value))
     const handleChange3 = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
+    useEffect(() => {
+        if (age == 66) {
+            toast("Achtung, Du bist alt geworden.", {
+                closeButton: true,
+                dismissible: true,
+                duration: 5_000
+            })
+        }
+    }, [age])
     return (
         <div className="grow m-8 flex flex-col gap-2">
             <label htmlFor="i1">Name</label>
-            <Input id="i1" value={name} 
-                onChange={handleChange1} 
-                />
+            <Input id="i1" value={name}
+                onChange={handleChange1}
+            />
             <label htmlFor="i2">Age2</label>
             <Input id="i2" value={age} type="number"
-                onChange={handleChange2} 
-                />
+                onChange={handleChange2}
+            />
+            {age > 100 && (<div className="text-red-500 text-xs">ZU ALT</div>)}
             <label htmlFor="i3">E-Mail</label>
-            <Input id="i3" value={email} type="email"
-                onChange={handleChange3} 
-                />
+            <Input disabled={age > 100} id="i3" value={email} type="email"
+                onChange={handleChange3}
+            />
+
+            <Toaster />
         </div>
     )
 }
